719dfbb7f8b299a5f70885c88ac8a4df86929a24
mor3245 <129242379+mor3245@users.noreply.github.com>
2026-08-25 15:37:36 +0800

    Add function to play 5 rounds
    This commit also adds a score tracker and modifications to
    accommodate it.

--- script.js
+++ script.js
@@ -21,14 +21,35 @@ function getHumanChoice() {
 function playRound(humanChoice, computerChoice)  {
 
     if(humanChoice === "ROCK" && computerChoice === "SCISSORS") {
-        console.log("You Win! Rock beats scissors.");
+        humanScore = humanScore + 1;
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You won this round! Rock beats scissors.`);
     } else if (humanChoice === "PAPER" && computerChoice === "ROCK" ) {
-        console.log("You Win! Paper beats rock.");
+        humanScore = humanScore + 1;
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You won this round! Paper beats rock.`);
     } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
-        console.log("You Win! Scissors beats paper.");
+        humanScore = humanScore + 1;        
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You won this round! Scissors beats paper.`);
     } else if (humanChoice === computerChoice) {
-      console.log("Its a tie!");  
+      console.log(`You: ${humanScore} | Computer: ${computerScore} No one won this round!`);  
     } else {
-        console.log(`You lost! ${computerChoice.charAt(0) + computerChoice.slice(1).toLowerCase()} beats ${humanChoice.charAt(0) + humanChoice.slice(1).toLowerCase()}.`)
+        computerScore = computerScore + 1;
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You lost this round! ${computerChoice.charAt(0) + computerChoice.slice(1).toLowerCase()} beats ${humanChoice.charAt(0) + humanChoice.slice(1).toLowerCase()}.`)
     }
+}
+
+function playGame() {
+    for(let i = 0; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You Win!`);
+    } else if (humanScore < computerScore) {
+        console.log(`You: ${humanScore} | Computer: ${computerScore} You Lost!`)
+    } else {
+        console.log(`You: ${humanScore} | Computer: ${computerScore} Its a tie!`);
+    }
+    
+    humanScore = 0;
+    computerScore = 0;
 }
