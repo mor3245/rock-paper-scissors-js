c681c73307c1a82439500d6dc6e778ada3752523
mor3245 <129242379+mor3245@users.noreply.github.com>
2026-08-25 15:20:53 +0800

    Add function to play one round
    The function should be called with humanChoice() and computerChoice()
    as its parameters.
    This allows the player to play one round of rock, paper, scissors.

--- script.js
+++ script.js
@@ -2,9 +2,33 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3 );
+    switch (Math.floor(Math.random() * 3 )) {
+        case 0:
+            return "ROCK";
+        case 1:
+            return "PAPER";
+        case 2:
+            return "SCISSORS";
+        default:
+            return null;
+    }
 }
 
 function getHumanChoice() {
-    return prompt("Enter rock, paper or scissors.");
+    return prompt("Enter rock, paper or scissors.").toUpperCase();
+}
+
+function playRound(humanChoice, computerChoice)  {
+
+    if(humanChoice === "ROCK" && computerChoice === "SCISSORS") {
+        console.log("You Win! Rock beats scissors.");
+    } else if (humanChoice === "PAPER" && computerChoice === "ROCK" ) {
+        console.log("You Win! Paper beats rock.");
+    } else if (humanChoice === "SCISSORS" && computerChoice === "PAPER") {
+        console.log("You Win! Scissors beats paper.");
+    } else if (humanChoice === computerChoice) {
+      console.log("Its a tie!");  
+    } else {
+        console.log(`You lost! ${computerChoice.charAt(0) + computerChoice.slice(1).toLowerCase()} beats ${humanChoice.charAt(0) + humanChoice.slice(1).toLowerCase()}.`)
+    }
 }
